2fa4ef294f26b3dd7eb6a2d4292add1457056e46
Ali I Samji <ali.samji@outlook.com>
2017-07-03 18:20:47 -0500

    Implement email caching methods.

--- src/cache.py
+++ src/cache.py
@@ -8,6 +8,7 @@ import exceptions
 
 # TODO: add variables or config file to define what data in the cache is too old.
 # TODO: add converter and adapter to translate datetime objects from/to python and sqlite3
+# TODO: add converter and adapter to translate bool values from/to python and sqlite3
 
 
 def getCache():
@@ -30,8 +31,9 @@ class Cache:
     # Class constants
     WEBPAGE_GET_STATEMENT = 'SELECT * FROM webpages WHERE url=?'
     WEBPAGE_SET_STATEMENT = 'INSERT OR REPLACE INTO webpages VALUES (?, ?, ?)'
-    EMAIL_GET_STATEMENT = 'SELECT * FROM emails WHERE url=?'
-    EMAIL_SET_STATEMENT = 'INSERT OR REPLACE INTO emails VALUES (?, ?, ?)'
+    EMAIL_GET_STATEMENT = 'SELECT * FROM emails WHERE address=?'
+    EMAIL_SET_STATEMENT = 'INSERT OR REPLACE INTO emails VALUES (?, ?, ?, ?)'
+    EMAIL_API_ENDPOINT = 'http://api.quickemailverification.com/v1/verify?email={:s}&apikey=e7c512323e3d0025bc7a94e59801abc1dc2f4a2d12ed295fef3b400b9e55' # noqa
     DB_MANAGEMENT_SCRIPTS = ["""
                              CREATE TABLE webpages (
                                 url TEXT PRIMARY KEY NOT NULL,
@@ -41,6 +43,7 @@ class Cache:
 
                              CREATE TABLE emails (
                                 address TEXT PRIMARY KEY NOT NULL,
+                                is_valid BOOL NOT NULL,
                                 reason TEXT NOT NULL,
                                 last_lookup DATETIME NOT NULL
                              );
@@ -116,7 +119,14 @@ class Cache:
 
         Verify the validity of address by sending it a test email.
         """
-        pass
+        address = str(address)
+        response = requests.get(self.EMAIL_API_ENDPOINT.format(address))
+        results = response.json()
+        self._database.execute(self.EMAIL_SET_STATEMENT,
+                               (address, 0 if results['safe_to_send'] == 'false' else 1,
+                                results['reason'], datetime.datetime.today()))
+        self._database.commit()
+        response.close()
 
     def get_email(self, address, *, nolookup=False):
         """Get the validity of the address.
@@ -125,7 +135,19 @@ class Cache:
         use lookup_email to lookup the validity online if it is not in the cache or
         if the data in the cache is too old.
         """
-        pass
+        address = str(address)
+        nolookup = bool(nolookup)
+        response = self._database.execute(self.EMAIL_GET_STATEMENT, (address,)).fetchone()
+        try:
+            info = InfoHolder(address=response[0], is_valid=response[1],
+                              reason=response[2], last_lookup=response[3])
+            return info
+        except TypeError:
+            if nolookup:
+                raise exceptions.CacheMissException(address) from None
+            else:
+                self.lookup_email(address)
+                return self.get_email(address, nolookup=True)
 
     def set_email(self, address, is_valid):
         """Manually set the validity of the address.
@@ -133,4 +155,10 @@ class Cache:
         Manually add or update the validity of the address in the cache. The
         is_valid must be a boolean value indicating the validity.
         """
-        pass
+        address = str(address)
+        is_valid = bool(is_valid)
+        reason = 'user_verified' if is_valid else 'user_refuted'
+        self._database.execute(self.EMAIL_SET_STATEMENT,
+                               (address, is_valid, reason,
+                                datetime.datetime.today()))
+        self._database.commit()
