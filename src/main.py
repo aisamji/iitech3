9358002328f09693b716f78e859a2378a02d03f3
Ali I Samji <ali.samji@outlook.com>
2017-07-10 14:27:16 -0500

    Make CLI more user-friendly.
    Add iitech3 symlink at project root directory for easy access.
    Add save-commands.json for Atom IDE to auto-run flake8.
    Add help text for all CLI options.

--- src/main.py
+++ src/main.py
@@ -62,62 +62,134 @@ def mark_url(args):
 def main(args=None):
     """Run the program with the given args or from the cmd args."""
     # Define base parser
-    base = argparse.ArgumentParser(prog='iitech3')
+    base = argparse.ArgumentParser(prog='iitech3',
+                                   description='An HTML newsletter manipulation utility '
+                                               'for the Ismaili Insight newsletter.',
+                                   formatter_class=argparse.RawTextHelpFormatter,
+                                   usage='%(prog)s -h|--help\n       '
+                                         '%(prog)s -v|--version\n       '
+                                         '%(prog)s <command>')
+    base._optionals.title = 'options'
     base.set_defaults(func=lambda x: base.print_help())
-    base_childs = base.add_subparsers()
+    base.add_argument('-v', '--version', action='version', version='%(prog)s {:s}'.format(__version__))
+    base_childs = base.add_subparsers(title='commands',
+                                      help='review\tReview and fix the HTML code of the newsletter in-place.\n'
+                                           'lookup\tLookup the status of an email or a url.\n'
+                                           'mark  \tManually mark the status of an email or a url.')
 
     # Define review parser
-    review_cmd = base_childs.add_parser('review')
+    review_cmd = base_childs.add_parser('review', prog='iitech3 review',
+                                        description='Review and fix the HTML code of the newsletter in-place.',
+                                        usage='%(prog)s <file>\n       '
+                                              '%(prog)s -p|--pasteboard')
+    review_cmd._optionals.title = 'options'
     review_cmd.set_defaults(func=review)
-    review_target = review_cmd.add_mutually_exclusive_group(required=True)
-    review_target.add_argument('file', action='store', type=str, nargs='?')
-    review_target.add_argument('-p', '--pasteboard', action='store_const',
-                               dest='file', const=None)
+    review_target_grp = review_cmd.add_argument_group(title='targets')
+    review_target_mex = review_target_grp.add_mutually_exclusive_group(required=True)
+    review_target_mex.add_argument('file', action='store', type=str, nargs='?',
+                                   help='The file that contains the HTML code to review.')
+    review_target_mex.add_argument('-p', '--pasteboard', action='store_const',
+                                   dest='file', const=None,
+                                   help='Specifies that the HTML code to review is on the pasteboard.')
 
     # Define lookup parser
-    lookup_cmd = base_childs.add_parser('lookup')
+    lookup_cmd = base_childs.add_parser('lookup', prog='iitech3 lookup',
+                                        description='Lookup the status of an email or a url.',
+                                        formatter_class=argparse.RawTextHelpFormatter,
+                                        usage='%(prog)s email [OPTIONS]\n       '
+                                              '%(prog)s webpage [OPTIONS]')
+    lookup_cmd._optionals.title = 'options'
     lookup_cmd.set_defaults(func=lambda x: lookup_cmd.print_help())
-    lookup_childs = lookup_cmd.add_subparsers()
+    lookup_childs = lookup_cmd.add_subparsers(title='types',
+                                              help='email  \tLook up the status of an email.\n'
+                                                    'webpage\tLook up the status of a webpage.')
 
-    lookup_email_cmd = lookup_childs.add_parser('email')
+    lookup_email_cmd = lookup_childs.add_parser('email', prog='iitech3 lookup email', add_help=False,
+                                                description='Look up the status of an email.',
+                                                usage='%(prog)s [-c|--cached] <address>\n       '
+                                                      '%(prog)s -f|--forced <address>')
     lookup_email_cmd.set_defaults(func=lookup_email)
-    lookup_email_mode = lookup_email_cmd.add_mutually_exclusive_group()
-    lookup_email_mode.add_argument('-c', '--cached', action='store_true')
-    lookup_email_mode.add_argument('-f', '--forced', action='store_true')
-    lookup_email_cmd.add_argument('address', action='store', type=str)
+    lookup_email_mode_grp = lookup_email_cmd.add_argument_group(title='modifiers')
+    lookup_email_mode_mex = lookup_email_mode_grp.add_mutually_exclusive_group()
+    lookup_email_mode_mex.add_argument('-c', '--cached', action='store_true',
+                                       help='Prevents an online lookup even if the status is not in the cache.')
+    lookup_email_mode_mex.add_argument('-f', '--forced', action='store_true',
+                                       help='Forces an online lookup before retrieving the status.')
+    lookup_email_gen_grp = lookup_email_cmd.add_argument_group(title='arguments')
+    lookup_email_gen_grp.add_argument('address', action='store', type=str,
+                                      help='The email address to lookup.')
+    lookup_email_gen_grp.add_argument('-h', '--help', action='help',
+                                      help='Print this help message and exit.')
 
-    lookup_url_cmd = lookup_childs.add_parser('webpage')
+    lookup_url_cmd = lookup_childs.add_parser('webpage', prog='iitech3 lookup webpage', add_help=False,
+                                              description='Look up the status of a webpage.',
+                                              usage='%(prog)s [-c|--cached] <url>\n       '
+                                                    '%(prog)s -f|--forced <url>')
     lookup_url_cmd.set_defaults(func=lookup_url)
-    lookup_url_mode = lookup_url_cmd.add_mutually_exclusive_group()
-    lookup_url_mode.add_argument('-c', '--cached', action='store_true')
-    lookup_url_mode.add_argument('-f', '--forced', action='store_true')
-    lookup_url_cmd.add_argument('url', action='store', type=str)
+    lookup_url_mode_grp = lookup_url_cmd.add_argument_group(title='modifiers')
+    lookup_url_mode_mex = lookup_url_mode_grp.add_mutually_exclusive_group()
+    lookup_url_mode_mex.add_argument('-c', '--cached', action='store_true',
+                                     help='Prevents an online lookup even if the status is not in the cache.')
+    lookup_url_mode_mex.add_argument('-f', '--forced', action='store_true',
+                                     help='Forces an online lookup before retrieving the status.')
+    lookup_url_gen_grp = lookup_url_cmd.add_argument_group(title='arguments')
+    lookup_url_gen_grp.add_argument('url', action='store', type=str,
+                                    help='The url to lookup.')
+    lookup_url_gen_grp.add_argument('-h', '--help', action='help',
+                                    help='Print this help message and exit.')
 
     # Define mark parser
-    mark_cmd = base_childs.add_parser('mark')
+    mark_cmd = base_childs.add_parser('mark', prog='iitech3 mark',
+                                      formatter_class=argparse.RawTextHelpFormatter,
+                                      description='Manually mark the status of an email or a url.',
+                                      usage='%(prog)s email [OPTIONS]\n       '
+                                            '%(prog)s webpage [OPTIONS]')
+    mark_cmd._optionals.title = 'arguments'
     mark_cmd.set_defaults(func=lambda x: mark_cmd.print_help())
-    mark_childs = mark_cmd.add_subparsers()
+    mark_childs = mark_cmd.add_subparsers(title='types',
+                                          help='email  \tManually mark the status of an email.\n'
+                                               'webpage\tManually mark the status of a webpage.')
 
-    mark_email_cmd = mark_childs.add_parser('email')
+    mark_email_cmd = mark_childs.add_parser('email', prog='iitech3 mark email', add_help=False,
+                                            description='Manually mark the status of an email.',
+                                            usage='%(prog)s --valid <address>\n       '
+                                                  '%(prog)s --invalid <address>')
     mark_email_cmd.set_defaults(func=mark_email)
-    mark_email_result = mark_email_cmd.add_mutually_exclusive_group(required=True)
-    mark_email_result.add_argument('--valid', action='store_true', dest='is_valid')
-    mark_email_result.add_argument('--invalid', action='store_false', dest='is_valid')
-    mark_email_cmd.add_argument('address', action='store', type=str)
+    mark_email_result_grp = mark_email_cmd.add_argument_group(title='statuses')
+    mark_email_result_mex = mark_email_result_grp.add_mutually_exclusive_group(required=True)
+    mark_email_result_mex.add_argument('--valid', action='store_true', dest='is_valid',
+                                       help='Mark the status of the address as valid.')
+    mark_email_result_mex.add_argument('--invalid', action='store_false', dest='is_valid',
+                                       help='Mark the status of the address as invalid.')
+    mark_email_gen_grp = mark_email_cmd.add_argument_group(title='arguments')
+    mark_email_gen_grp.add_argument('address', action='store', type=str,
+                                    help='The address to mark.')
+    mark_email_gen_grp.add_argument('-h', '--help', action='help',
+                                    help='Print this help message and exit.')
 
-    mark_url_cmd = mark_childs.add_parser('webpage')
+    mark_url_cmd = mark_childs.add_parser('webpage', prog='iitech3 mark webpage', add_help=False,
+                                          description='Manually mark the status of a webpage.',
+                                          usage='%(prog)s -s|--status STATUS <url>\n       '
+                                                '%(prog)s -k|--ok <url>\n       '
+                                                '%(prog)s -b|--bad <url>\n       '
+                                                '%(prog)s -d|--down <url>')
     mark_url_cmd.set_defaults(func=mark_url)
-    mark_url_status = mark_url_cmd.add_mutually_exclusive_group(required=True)
-    mark_url_status.add_argument('-s', '--status', action='store', type=int)
-    mark_url_status.add_argument('-k', '--ok', action='store_const', const=200,
-                                 dest='status')
-    mark_url_status.add_argument('-b', '--bad', action='store_const', const=400,
-                                 dest='status')
-    mark_url_status.add_argument('-d', '--down', action='store_const', const=500,
-                                 dest='status')
-    mark_url_status.add_argument('--teapot', action='store_const', const=418,
-                                 dest='status')
-    mark_url_cmd.add_argument('url', action='store', type=str)
+    mark_url_status_grp = mark_url_cmd.add_argument_group(title='statuses')
+    mark_url_status_mex = mark_url_status_grp.add_mutually_exclusive_group(required=True)
+    mark_url_status_mex.add_argument('-s', '--status', action='store', type=int,
+                                     help='Mark the url with the given HTTP status code.')
+    mark_url_status_mex.add_argument('-k', '--ok', action='store_const', const=200,
+                                     dest='status', help='Mark the url as OK (status: 200).')
+    mark_url_status_mex.add_argument('-b', '--bad', action='store_const', const=400,
+                                     dest='status', help='Mark the url as a client error (status: 400).')
+    mark_url_status_mex.add_argument('-d', '--down', action='store_const', const=500,
+                                     dest='status', help='Mark the url as a server error (status: 500).')
+    mark_url_status_mex.add_argument('--teapot', action='store_const', const=418,
+                                     dest='status', help='Mark the url as a teapot (status: 418).')
+    mark_url_gen_grp = mark_url_cmd.add_argument_group(title='arguments')
+    mark_url_gen_grp.add_argument('url', action='store', type=str, help='The url to mark.')
+    mark_url_gen_grp.add_argument('-h', '--help', action='help',
+                                  help='Print this help message and exit.')
 
     # Parse args
     definition = base.parse_args(args)
